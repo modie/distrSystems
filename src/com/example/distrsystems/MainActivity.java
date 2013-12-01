9ccb9e46c7a66754e1c57be4346fa4c59b5f58ae
modie <moodyr64@gmail.com>
2013-12-01 19:41:45 +0200

    commit

--- src/com/example/distrsystems/MainActivity.java
+++ src/com/example/distrsystems/MainActivity.java
@@ -1,39 +1,37 @@
 package com.example.distrsystems;
 
-// what up?
-//still testing
-// it should work
-// dafuq
-//fixed 
-// trying git for windows
-
-/*
- * 1
- * 2
- * 3
- * 4
- * 5
- * 6
- * 7
- * 8
- * 9
- * 10
- * 11
- * 12
- */
-
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.app.Activity;
+import android.text.format.Formatter;
+import android.util.Log;
 import android.view.Menu;
+import android.widget.TextView;
 
 public class MainActivity extends Activity
 {
 
+	TextView TvIp ;
+	String ip ;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		try
+		{
+			WifiManager wm = (WifiManager) getSystemService(WIFI_SERVICE);
+			ip = Formatter.formatIpAddress(wm.getConnectionInfo().getIpAddress());
+		}
+		catch(Exception e)
+		{
+			Log.e("error","could not find ip adress");
+		}
+		TvIp = (TextView) findViewById(R.id.tvip);
+		TvIp.setText(ip);
+		
+		
 	}
 
 	@Override
